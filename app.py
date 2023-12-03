f5f941edcee46ac9b1add6776001ce2a1a062859
henrywangballer <60195887+henrywangballer@users.noreply.github.com>
2023-12-03 17:45:51 -0600

    progress5

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ from cdk.s3_stack import S3Stack
 from cdk.dynamodb_lambda_stack import DynamodbLambdaStack
 from cdk.lambda_layer_stack import LambdaLayerStack
 from cdk.rekognition_lambda_s3_trigger_stack import RekognitionLambdaS3TriggerStack
+from cdk.sqs_sns_stack import S3SnsSqsLambdaChainStack
+from cdk.ec2_stack import EC2InstanceStack
+from cdk.kinesis_stack import LambdaWithKinesisTrigger
+from cdk.kinesis_s3_stack import KinesisS3
 
 app = cdk.App()
 
@@ -15,5 +19,10 @@ ApiCorsLambdaStack(app, "ApiCorsLambdaStack",)
 S3Stack(app, "S3Stack")
 DynamodbLambdaStack(app, 'DynamodbLambdaStack')
 LambdaLayerStack(app, 'LambdaLayerStack')
-RekognitionLambdaS3TriggerStack(app, 'RekognitionLambdaS3TriggerStack')
+# RekognitionLambdaS3TriggerStack(app, 'RekognitionLambdaS3TriggerStack')
+S3SnsSqsLambdaChainStack(app, 'S3SnsSqsLambdaChainStack')
+EC2InstanceStack(app, "Ec2Stack")
+LambdaWithKinesisTrigger(app, "LambdaWithKinesisTrigger")
+KinesisS3(app, 'KinesisS3')
+
 app.synth()
